5249f7c311960302c1b94bfdc2190c69cefb5bb2
Ieuan-Vinluan <b2025idrvinluan@pshs.edu.ph>
2023-11-24 00:55:51 +0800

    added to skeleton of main page

--- src/App.jsx
+++ src/App.jsx
@@ -57,7 +57,12 @@ function App() {
         <div className="content-container">
           <ClickableDropdown headerText="Physical Quantities" description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus ipsa, beatae ipsam odio exercitationem necessitatibus. Soluta, quia! Veniam, magni, incidunt, odit hic minima minus voluptatum assumenda quasi quae laboriosam fugit!">
             { /* clickable dropdown item here */ }
-            <a href="#">Meowww</a>
+          </ClickableDropdown>
+          <ClickableDropdown headerText="Kinematics" description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam sapiente similique, neque deserunt iste, quis voluptate sint cupiditate esse eos, nobis expedita ullam recusandae itaque ea aliquid modi. Rerum, necessitatibus?">
+            { /* items here */ }
+          </ClickableDropdown>
+          <ClickableDropdown headerText="Vectors" description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus id mollitia odio est quod tempore assumenda tenetur veritatis recusandae dolorem. Ab quasi magni illo eum quibusdam excepturi facere, praesentium unde sint, porro inventore eveniet deserunt saepe laboriosam est doloremque fugiat aut perferendis suscipit! Aut dignissimos commodi aliquam libero rem veniam culpa necessitatibus quisquam nostrum non, asperiores inventore nam ea eum quaerat. Facilis earum rem rerum, pariatur iure adipisci modi recusandae.">
+            { /* items here */}
           </ClickableDropdown>
         </div>
       </main>
@@ -68,7 +73,11 @@ function App() {
 function ClickableDropdown(props) {
   return (
     <div className="clickable-dropdown">
-      <h2 className="clickable-dropdown-header">{props.headerText}</h2>
+      <div className="clickable-dropdown-header-container">
+        <h2 className="clickable-dropdown-header">{props.headerText}</h2>
+        { /* TODO replace --&gt; with actual arrow image */}
+        <a href="#">--&gt;</a>
+      </div>
       <div className="clickable-dropdown-body">
         <p className="clickable-dropdown-description">
           {props.description}
