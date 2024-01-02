eb98dbabbaf4faf378cb6f2f0365c2e58fa2889e
Ieuan-Vinluan <b2025idrvinluan@pshs.edu.ph>
2024-01-02 20:44:05 +0800

    testing latex

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,11 @@ import './css/App.css';
 import HomeScreen from './HomeScreen';
 import LessonPage from './LessonPage';
 import StandardHeader from './StandardHeader';
+import Latex from "react-latex-next";
+import "katex/dist/katex.min.css";
 
 function App() {
   const [curScreen, setCurScreen] = useState('home');
-
   function goTo() {
     if (curScreen == 'home') {
       return (<HomeScreen setCurScreen={setCurScreen}/>);
@@ -14,7 +15,12 @@ function App() {
       return (
         <LessonPage setCurScreen={setCurScreen}>
           <h1>Lesson 1</h1>
-          { /* lesson content here */ }
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur vel accusamus vitae omnis, ducimus iure voluptatum laborum reiciendis exercitationem eligendi! Suscipit culpa soluta cupiditate nesciunt quibusdam neque adipisci eveniet voluptates!</p>
+          <div className="lesson-one-equation-one"> { /* testing equation */ }
+            <Latex>
+              $\Delta x = x - x_0$
+            </Latex>
+          </div>
         </LessonPage>
       );
     } else if (curScreen == 'lesson2') {
